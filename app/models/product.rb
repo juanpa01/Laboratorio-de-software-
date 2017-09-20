f8f6f1742e5b2e46050f4d8ceabfa821635a49d1
juanpa01 <juan-pablo961115@utp.edu.co>
2017-09-20 14:55:59 -0500

    validaciones agregadas y modificaciones de la vista de productos

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   has_many :order
+  validates :nombre, presence: true;
+  validates :descripcion, presence: true;
+  validates :marca, presence: true;
+  validates :categoria, presence: true;
+  validates :cantidad, presence: true;
+  validates :precio, presence: true;
 end
