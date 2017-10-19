38c2b349a3ba8d2c1094b49a3dd16c9aa6995429
juanpa01 <juan-pablo961115@utp.edu.co>
2017-10-18 21:12:46 -0500

    agregado nuevos campos de usuario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permit_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permit_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre, :direccion, :telefono])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nombre, :direccion, :telefono])
+  end
+
 end
