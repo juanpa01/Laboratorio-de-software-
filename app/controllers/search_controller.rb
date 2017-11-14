05c4291cfdbab2d84e83e89806b895aa79def0b0
juanpa01 <juan-pablo961115@utp.edu.co>
2017-11-14 16:05:52 -0500

    C - busqueda en intervalos de fechas

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,11 @@
 class SearchController < ApplicationController
   def create
-    palabra = "%#{params[:keyword]}%"
+    date_start = "#{params[:date_start]}"
+    date_end = "#{params[:date_end]}"
 
-    @products = Product.where("nombre LIKE ? OR descripcion LIKE ? OR marca LIKE ? OR categoria LIKE ?", palabra, palabra, palabra, palabra)
-
+    #@products = Product.where("nombre LIKE ? OR descripcion LIKE ? OR marca LIKE ? OR categoria LIKE ?", palabra, palabra, palabra, palabra)
+    @products = Product.where("created_at BETWEEN ? AND ?",date_start, date_end)
+    #@products = Product.where(:created_at => date_start..date_end)
     respond_to do |format|
       format.html {redirect_to root_path}
       format.json { render json: @products }
